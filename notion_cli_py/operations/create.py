bbab1048ed6b4c1be842c04b8e3eca505f87c8e3
Tomonori HIRATA <hirata@sqlab.jp>
2022-03-21 02:34:00 +0900

    update: create, update

--- notion_cli_py/operations/create.py
+++ notion_cli_py/operations/create.py
@@ -3,7 +3,7 @@ from typing import Dict
 from ..client import client
 import sys
 import re
-from ..utils import read_file
+from ..utils import read_file, confirm
 from tabulate import tabulate
 
 class CreateClass:
@@ -11,7 +11,9 @@ class CreateClass:
         """ CreateClass __init__ """
 
     def pages(self, page_ids="", database_ids="", template_name="simple_page", read_path=None, noconfirm=False, label="current"):
-        """ create pages """
+        """
+        create pages
+        """
         c = client.Client(label)
 
         ### load template
@@ -21,38 +23,25 @@ class CreateClass:
 
         ret = []
         for page_id in page_ids.split():
-            while True and (not noconfirm):
-                contents = [("page_id", page_id), ("template path", read_path)]
-                print(tabulate(contents, headers=["key", "value"], tablefmt='fancy_grid'))
-                choice = input("is OK? [y/N]: ").lower()
-                if choice in ['y', 'ye', 'yes']:
-                    payload["parent"] = {
-                        "page_id": page_id,
-                    }
-                    ret.append(json.loads(c.create_page(payload)))
-                    break
-                elif choice in ['n', 'no']:
-                    print("==> Skipped.")
-                    break
+            contents = [("page_id", page_id), ("template path", read_path)]
+            if confirm.confirm(contents, noconfirm=noconfirm):
+                payload["parent"] = {
+                    "page_id": page_id,
+                }
+                ret.append(json.loads(c.create_page(payload)))
         for database_id in database_ids.split():
-            # confirm settings
-            while True and (not noconfirm):
-                contents = [("database_id", database_id), ("template path", read_path)]
-                print(tabulate(contents, headers=["key", "value"], tablefmt='fancy_grid'))
-                choice = input("is OK? [y/N]: ").lower()
-                if choice in ['y', 'ye', 'yes']:
-                    payload["parent"] = {
-                        "database_id": database_id,
-                    }
-                    ret.append(json.loads(c.create_page(payload)))
-                    break
-                elif choice in ['n', 'no']:
-                    print("==> Skipped.")
-                    break
+            contents = [("database_id", database_id), ("template path", read_path)]
+            if confirm.confirm(contents, noconfirm=noconfirm):
+                payload["parent"] = {
+                    "database_id": database_id,
+                }
+                ret.append(json.loads(c.create_page(payload)))
         return json.dumps(ret) if len(ret) != 0 else sys.exit(0)
 
-    def databases(self, page_ids="", template_name="simple_database", read_path=None, noconfirm=False, label="current"):
-        """ create a single database """
+    def databases(self, page_ids, template_name="simple_database", read_path=None, noconfirm=False, label="current"):
+        """
+        create a single database
+        """
         c = client.Client(label)
 
         ### load template
@@ -62,18 +51,12 @@ class CreateClass:
 
         ret = []
         for page_id in page_ids.split():
-            while True and (not noconfirm):
-                contents = [("page_id", page_id), ("template name", read_path)]
-                print(tabulate(contents, headers=["key", "value"], tablefmt='fancy_grid'))
-                choice = input("is OK? [y/N]: ").lower()
-                if choice in ['y', 'ye', 'yes']:
-                    payload["parent"] = {
-                        "page_id": page_id,
-                    }
-                    ret.append(json.loads(c.create_database(payload)))
-                    break
-                elif choice in ['n', 'no']:
-                    print("==> Skipped.")
-                    break
+            contents = [("page_id", page_id), ("template name", read_path)]
+            if confirm.confirm(contents, noconfirm=noconfirm):
+                payload["parent"] = {
+                    "type": "page_id",
+                    "page_id": page_id,
+                }
+                ret.append(json.loads(c.create_database(payload)))
 
         return json.dumps(ret) if len(ret) != 0 else sys.exit(0)
--- notion_cli_py/operations/update.py
+++ notion_cli_py/operations/update.py
@@ -1,6 +1,6 @@
 import json
 from ..client import client
-from ..utils import read_file
+from ..utils import read_file, confirm
 from tabulate import tabulate
 import sys
 
@@ -8,9 +8,12 @@ class UpdateClass:
     def __init__(self):
         """ UpdateClass __init__ """
 
-    def pages(self, page_ids="", template_name="simple_page", read_path=None, noconfirm=False, label="current"):
-        """ update pages """
+    def pages(self, page_ids, template_name="simple_page", read_path=None, noconfirm=False, label="current"):
+        """
+        update pages
+        """
         c = client.Client(label)
+
         ### load template
         if read_path is None:
             read_path = read_file.read_template_file(template_name)
@@ -18,22 +21,18 @@ class UpdateClass:
 
         ret = []
         for page_id in page_ids.split():
-            while True and (not noconfirm):
-                contents = [("page_id", page_id), ("template path", read_path)]
-                print(tabulate(contents, headers=["key", "value"], tablefmt='fancy_grid'))
-                choice = input("is OK? [y/N]: ").lower()
-                if choice in ['y', 'ye', 'yes']:
-                    ret.append(json.loads(c.update_page(page_id, payload)))
-                    break
-                elif choice in ['n', 'no']:
-                    print("==> Skipped.")
-                    break
+            contents = [("page_id", page_id), ("template path", read_path)]
+            if confirm.confirm(contents, noconfirm=noconfirm):
+                ret.append(json.loads(c.update_page(page_id, payload)))
 
         return json.dumps(ret) if len(ret) != 0 else sys.exit(0)
 
-    def databases(self, database_ids="", template_name="simple_database", read_path=None, noconfirm=False, label="current"):
-        """ update databases """
+    def databases(self, database_ids, template_name="simple_database", read_path=None, noconfirm=False, label="current"):
+        """
+        update databases
+        """
         c = client.Client(label)
+
         ### load template
         if read_path is None:
             read_path = read_file.read_template_file(template_name)
@@ -41,22 +40,18 @@ class UpdateClass:
 
         ret = []
         for database_id in database_ids.split():
-            while True and (not noconfirm):
-                contents = [("database_id", database_id), ("template path", read_path)]
-                print(tabulate(contents, headers=["key", "value"], tablefmt='fancy_grid'))
-                choice = input("is OK? [y/N]: ").lower()
-                if choice in ['y', 'ye', 'yes']:
-                    ret.append(json.loads(c.update_database(database_id, payload)))
-                    break
-                elif choice in ['n', 'no']:
-                    print("==> Skipped.")
-                    break
+            contents = [("database_id", database_id), ("template path", read_path)]
+            if confirm.confirm(contents, noconfirm=noconfirm):
+                ret.append(json.loads(c.update_database(database_id, payload)))
 
         return json.dumps(ret) if len(ret) != 0 else sys.exit(0)
 
-    def blocks(self, block_ids="", template_name="simple_block", read_path=None, noconfirm=False, label="current"):
-        """ update blocks """
+    def blocks(self, block_ids, template_name="simple_block", read_path=None, noconfirm=False, label="current"):
+        """
+        update blocks
+        """
         c = client.Client(label)
+
         ### load template
         if read_path is None:
             read_path = read_file.read_template_file(template_name)
@@ -64,15 +59,8 @@ class UpdateClass:
 
         ret = []
         for block_id in block_ids.split():
-            while True and (not noconfirm):
-                contents = [("block_id", block_id), ("template path", read_path)]
-                print(tabulate(contents, headers=["key", "value"], tablefmt='fancy_grid'))
-                choice = input("is OK? [y/N]: ").lower()
-                if choice in ['y', 'ye', 'yes']:
-                    ret.append(json.loads(c.update_block(block_id, payload)))
-                    break
-                elif choice in ['n', 'no']:
-                    print("==> Skipped.")
-                    break
+            contents = [("block_id", block_id), ("template path", read_path)]
+            if confirm.confirm(contents, noconfirm=noconfirm):
+                ret.append(json.loads(c.update_block(block_id, payload)))
 
         return json.dumps(ret) if len(ret) != 0 else sys.exit(0)
