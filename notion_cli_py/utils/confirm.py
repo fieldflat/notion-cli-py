14bf26c069cf32306394764bcd15d14d4f875c18
Tomonori HIRATA <hirata@sqlab.jp>
2022-04-30 23:08:24 +0900

    add: error handling

--- notion_cli_py/utils/confirm.py
+++ notion_cli_py/utils/confirm.py
@@ -1,11 +1,12 @@
 from tabulate import tabulate
+from ..utils import input_handler
 
 
 def confirm(contents, headers=["key", "value"], tablefmt='fancy_grid', noconfirm=False):
     if not noconfirm:
         print(tabulate(contents, headers=headers, tablefmt=tablefmt))
     while True and (not noconfirm):
-        choice = input("is OK? [y/N]: ").lower()
+        choice = input_handler.input_handler("is OK? [y/N]: ").lower()
         if choice in ['y', 'ye', 'yes']:
             return True
         elif choice in ['n', 'no']:
