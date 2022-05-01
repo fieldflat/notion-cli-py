19ca74ffc802bff2c2f679e23c9d310f84aa9353
Tomonori HIRATA <hirata@sqlab.jp>
2022-05-01 16:14:25 +0900

    fix: logging

--- notion_cli_py/client/client.py
+++ notion_cli_py/client/client.py
@@ -7,7 +7,7 @@ from ..utils import logger
 
 class Client:
     def __init__(self, label):
-        self.logger = logger.init_logger()
+        self.logger = logger.init_logger(__name__)
         try:
             PATH = os.environ['HOME'] + "/.notion_cli/config.toml"
             config = toml.load(open(PATH))
